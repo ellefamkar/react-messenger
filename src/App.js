3785e6c03458e1bc88fbba68b0e4973405016d72
Elle Famkar <ellefamkar@gmail.com>
2023-07-13 23:00:53 +0430

    feat : add navbar and chat section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { Switch, Route } from "react-router-dom/cjs/react-router-dom";
 import Login from "./components/Login";
+import Chats from "./components/Chats";
+
+// context 
+import AuthContextProvider from "./contexts/AuthContextProvider";
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
       <Switch>
-        <Route path="/login" component={Login} />  
-        <Route path="/" component={Login} />  
-      </Switch>     
+          <Route path="/chats" component={Chats} />  
+          <Route path="/login" component={Login} />  
+          <Route path="/" component={Login} />  
+        </Switch>
+    </AuthContextProvider>     
     </>
   );
 }
---
+++ src/contexts/AuthContextProvider.js
@@ -0,0 +1,30 @@
+import React , {useState, useEffect, createContext} from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import { auth } from '../firebase';
+
+const AuthContext = createContext();
+
+const AuthContextProvider = ({children}) => {
+
+    const [Loading, setLoading] = useState(true);
+    const [user, setUser] = useState(false);
+    const history = useHistory();
+
+    useEffect(()=>{
+        auth.onAuthStateChanged((user)=>{
+            setUser(user);
+            setLoading(false);
+            if(user) history.push("/chats");
+        })
+    }, [user, history]);
+
+    return (
+        <>
+            <AuthContext.Provider value={user}>
+                {!Loading && children}
+            </AuthContext.Provider>
+        </>
+    );
+};
+
+export default AuthContextProvider;
