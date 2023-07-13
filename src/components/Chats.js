71d6b229a3cae9c0080a010ab567cba436c9694c
Elle Famkar <ellefamkar@gmail.com>
2023-07-13 23:35:20 +0430

    feat : add chatEngine to the project

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 // Components
 import Navbar from './Navbar';
-
+import { auth } from '../firebase';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
 // Styles
 import styles from "./Chats.module.css"
 
 const Chats = () => {
+
+    const histroy = useHistory();
+
+    const logOutHandler = async () =>{
+        await auth.signOut();
+        histroy.push("/");
+    }
+
     return (
         <div className={styles.container}>
-            <Navbar />
+            <Navbar logOutHandler={logOutHandler} />
+            <ChatEngine
+                height="calc(100vh - 50px)"
+                projectId="c586e3d8-43ae-431d-86a0-baeb6a49fd73"
+                userName="."
+                userSecret="."
+            />
         </div>
     );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,13 @@ import React from 'react';
 // Styles
 import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+const Navbar = ({logOutHandler}) => {
     return (
         <div className={styles.container}>
             <div className={styles.name}>
                 SoloMates
             </div>
-            <div className={styles.logout}>
+            <div className={styles.logout} onClick={logOutHandler}>
                 Logout
             </div>
         </div>
